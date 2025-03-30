35045e346ce73f03e10a375ecc4bc2a04c28cd3e
behnam <bhnm_mm@yahoo.com>
2025-03-30 13:26:59 +0330

    add new file

---
+++ Python/calculator.py
@@ -0,0 +1,21 @@
+
+
+
+
+# calculator
+
+first_number = int(input("please enter your first number: "))
+action = input("plese choose one of this + - * /: ")
+second_number = int(input("please enter your second number: "))
+if action == "+":
+    print(first_number + second_number)
+if action == "-":
+    print(first_number - second_number)
+if action == "*":
+    print(first_number * second_number)
+if action == "/":
+    print(int(first_number / second_number))
+    
+    
+    
+       
